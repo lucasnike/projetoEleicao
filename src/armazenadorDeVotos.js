39bdbbdf278a688c0e54a97d88146b736580a55c
lucasnike <lucas4cristal@gmail.com>
2020-04-16 07:30:42 -0300

    Comentei todo o código, e imprimi o menu dentro de uma função

--- src/armazenadorDeVotos.js
+++ src/armazenadorDeVotos.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+let dados = require('./dados.json')
 
-
-function armazenar(candidateIndex, candidateArray) {
+// Verifica qual candidato foi escolhido, adiciona mais 1 voto e retorna o array modificado
+function armazenar(candidateIndex, candidateArray) { 
     if(candidateIndex === 1) candidateArray[candidateIndex - 1].votos += 1
     if(candidateIndex === 2) candidateArray[candidateIndex - 1].votos += 1
     if(candidateIndex === 3) candidateArray[candidateIndex - 1].votos += 1
@@ -14,30 +15,48 @@ function armazenar(candidateIndex, candidateArray) {
     return candidateArray
 }
 
+// Verfica opção de terminar votação e retorna o número 2 para quebrar o While
 function terminarWhile(candidateIndex) {
     if(candidateIndex == 9) return 2
     return 0
 }
 
+// Pega o resultado de um novo voto e salva no arquivo a cada repetição
 function substituirNoArquivo(dados) {
     fs.writeFileSync(__dirname + '/dados.json', JSON.stringify(dados))
     return dados
 }
 
+// Verifica qual candidato tem mais votos e retorna o vencedor
 function verificarVencedor(candidatesArray) {
     return candidatesArray[0].votos > candidatesArray[1].votos ? candidatesArray[0] : candidatesArray[1]
 }
 
+// Zera a quantidade de votos dos candidatos e armazena no arquivo
 function zerarVotos(dados) {
     for(let i = 0; i < dados.candidatos.length; i++){
         dados.candidatos[i].votos = 0
     }
     return substituirNoArquivo(dados)
 }
+
+function imprimirMenu() {
+    console.log('   ----------------------');
+    console.log(`   |[1] ${dados.candidatos[0].nome}       |`);
+    console.log('   ----------------------');
+    console.log(`   |[2] ${dados.candidatos[1].nome}  |`);
+    console.log('   ----------------------');
+    console.log(`   |[3] ${dados.candidatos[2].nome}            |`);
+    console.log('   ----------------------');
+    console.log('   |[9] Encerrar votação|');
+    console.log('   ----------------------\n');
+}
+// Exporta todas as funções para outros modulos
 module.exports = {
     armazenar,
     substituirNoArquivo,
     verificarVencedor,
     zerarVotos,
-    terminarWhile
+    terminarWhile,
+    imprimirMenu
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 const readline = require('readline-sync')
 const funcionalidades = require('./armazenadorDeVotos')
 let dados = require('./dados.json')
+
 console.log('~~~~~~VOTAÇÃO INICIADA~~~~~~\n');
+
 let i = 0
 while(2 > i){
-    console.log('   ----------------------');
-    console.log(`   |[1] ${dados.candidatos[0].nome}       |`);
-    console.log('   ----------------------');
-    console.log(`   |[2] ${dados.candidatos[1].nome}  |`);
-    console.log('   ----------------------');
-    console.log(`   |[3] ${dados.candidatos[2].nome}            |`);
-    console.log('   ----------------------');
-    console.log('   |[9] Encerrar votação|');
-    console.log('   ----------------------\n');
-    let choice = readline.questionInt('   Digite: ')
-    i = funcionalidades.terminarWhile(choice) 
+    funcionalidades.imprimirMenu() // imprime menu
+
+    let choice = readline.questionInt('   Digite: ') // Recebe dados do teclado
+
+    i = funcionalidades.terminarWhile(choice) // Testa se eu terminei o programa
     console.log('\n');
-    dados.candidatos = funcionalidades.armazenar(choice, dados.candidatos)
-    funcionalidades.substituirNoArquivo(dados)
+
+    dados.candidatos = funcionalidades.armazenar(choice, dados.candidatos) //Adiciona e armazena meu voto
+
+    funcionalidades.substituirNoArquivo(dados)// Sobescreve novo arquivo
 }
-const vencedor = funcionalidades.verificarVencedor(dados.candidatos)
-console.log(`O vencedor é ${vencedor.nome} com ${vencedor.votos}\n`);
-funcionalidades.zerarVotos(dados)
+const vencedor = funcionalidades.verificarVencedor(dados.candidatos) // Armazena o vencedor numa constante
+
+console.log(`O vencedor é ${vencedor.nome} com ${vencedor.votos}\n`);// Mostra quem venceu e a quantidade de votos
+
+funcionalidades.zerarVotos(dados)// Zera a quantidade de votos de todos os candidatos
+
 console.log('~~~~~~VOTAÇÃO ENCERRADA~~~~~~');
