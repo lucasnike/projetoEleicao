3e4237c511a0a2b91df55f94946ef85c012a7cdf
lucasnike <lucas4cristal@gmail.com>
2020-04-16 07:33:17 -0300

    Remove pasta de testes

--- tests/mostrandoMenu.js
+++
@@ -1,8 +0,0 @@
-    console.log('________________');
-    console.log(`|[1] Jose      |`);
-    console.log('________________');
-    console.log(`|[2] Alves     |`);
-    console.log('________________');
-    console.log(`|[3] Nulo      |`);
-    console.log('________________');
-    console.log(`|[4] Branco    |`);
